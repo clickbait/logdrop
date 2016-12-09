eb29d4aa22d114d1d131e81b16e1b89a1cff1b9d
Neko <cronhound@gmail.com>
2016-12-09 14:08:57 +1030

    initial commit of functional script

---
+++ index.js
@@ -0,0 +1,56 @@
+var Discord = require('discord.io');
+var fs = require('fs');
+
+var self = new Discord.Client({
+    token: process.env.DiscordToken,
+    autorun: true
+});
+
+self.on('ready', function() {
+    console.log(self.username + " - (" + self.id + ")");
+});
+
+self.on('message', function(user, userID, channelID, message, event) {
+    if (message === "!this" && userID == self.id) {
+    	console.log("Starting to dump log");
+
+    	log_messages(self, channelID);
+    }
+});
+
+function log_messages(self, channelID, before = null, log = "") {
+	var filepath = 'logs/' + channelID + '.txt';
+
+	self.getMessages({
+		channelID: channelID,
+		limit: 100,
+		before: before
+	}, function (err, messages) {
+		if (err) { console.log(err); return; }
+
+		before = messages.pop().id
+		messages = messages.reverse()
+
+		log = build_messages(self, messages) + log
+
+		console.log("Count: " + messages.length);
+
+		if (messages.length >= 99) {
+			log_messages(self, channelID, before, log);
+		} else {
+			fs.writeFile(filepath, log);
+			console.log("log saved to: " + filepath);
+		}
+	});
+}
+
+function build_messages(self, messages) {
+	var log_part = "";
+
+	for (var i=0; i < messages.length; i++) {
+		console.log("logging: " + messages[i].content)
+		log_part += messages[i].author.username + " : " + self.fixMessage(messages[i].content) + "\n"
+	}
+
+	return log_part;
+}
