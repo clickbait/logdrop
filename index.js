b9b833e95ae45cbed09a4e7f89e0f2a72fde67ae
neko <neko@pomf.is>
2016-12-29 08:18:24 +1030

    tidied log output

--- index.js
+++ index.js
@@ -49,7 +49,12 @@ function build_messages(self, messages) {
 
 	for (var i=0; i < messages.length; i++) {
 		console.log("logging: " + messages[i].id)
-		log_part += messages[i].author.username + " : " + self.fixMessage(messages[i].content) + "\n";
+
+    log_part += messages[i].author.username + " : ";
+
+    if (messages[i].content.length > 0) {
+		  log_part += self.fixMessage(messages[i].content) + "\n";
+    }
 
     for (var j=0; j < messages[i].attachments.length; j++) {
       log_part += messages[i].attachments[j].url + "\n";
